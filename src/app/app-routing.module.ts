c08553df2b2faede1f423e987433ca8e967eae70
joubits <joubits.dw@gmail.com>
2019-05-04 17:03:57 -0500

    Agregado Rutas y Hash#

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router'
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
+
+//RouterModule para trabajar con las rutas principales o rutas normales... NO rutas hijas!! 
+
+const app_routes: Routes = [
+    //cada ruta que se va a tener... Menu principal
+    { path: 'home', component: PortafolioComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'item', component: ItemComponent },
+    { path: '**', pathMatch: 'full', redirectTo: 'home' },
+
+
+];
+
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot( app_routes, { useHash: true } )
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+
+export class AppRoutingModule { }
